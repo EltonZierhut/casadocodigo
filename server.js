372124c3cec158746a339586af3e60cc7754f743
EltonZierhut <eltonzierhut@gmail.com>
2019-05-08 23:23:54 -0300

    Node e Express

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
+const express = require('express');
+const app=express();
+
+
+
 const http = require('http');
 
 const servidor = http.createServer(function(request, response){
-    response.end(`
+
+    let html;
+
+    if (request.url == "/"){
+        html = `
         <html>
             <head>
                 <meta charset="utf-8":
             </head>
             <body>
-                <h1> Casa do Código</h1>
+                <h1>Casa do Código</h1>
             </body>
         </html>
-    `);
+        `;
+    } else if (request.url == "/livros"){
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8":
+            </head>
+            <body>
+                <h1>Livros</h1>
+            </body>
+        </html>
+        `;
+    }
+
+    response.end(html);
 });
 servidor.listen(3000);
 
