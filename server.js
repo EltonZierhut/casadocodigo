794b9271fc252e2cd47ba1e5f18d2312adeec161
elton <elton@adapcon.com.br>
2019-05-09 13:19:18 -0300

    rotas

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const app=express();
+const app = require('./src/config/custom-express');
 
-
-
-const http = require('http');
-
-const servidor = http.createServer(function(request, response){
-
-    let html;
-
-    if (request.url == "/"){
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8":
-            </head>
-            <body>
-                <h1>Casa do Código</h1>
-            </body>
-        </html>
-        `;
-    } else if (request.url == "/livros"){
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8":
-            </head>
-            <body>
-                <h1>Livros</h1>
-            </body>
-        </html>
-        `;
-    }
-
-    response.end(html);
+app.listen(3000, function(){
+    console.log(`Servidor rodando na porta 3000`);
 });
-servidor.listen(3000);
+
+
+// const http = require('http');
+
+// const servidor = http.createServer(function(request, response){
+
+//     let html;
+
+//     if (request.url == "/"){
+//         html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8":
+//             </head>
+//             <body>
+//                 <h1>Casa do Código</h1>
+//             </body>
+//         </html>
+//         `;
+//     } else if (request.url == "/livros"){
+//         html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8":
+//             </head>
+//             <body>
+//                 <h1>Livros</h1>
+//             </body>
+//         </html>
+//         `;
+//     }
+
+//     response.end(html);
+// });
+// servidor.listen(3000);
 
---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,27 @@
+module.exports = (app) => {
+    app.get('/',function(request, response){
+        response.send(`
+             <html>
+                <head>
+                    <meta charset="utf-8":
+                </head>
+                <body>
+                    <h1>Casa do Código</h1>
+                </body>
+            </html>
+        `);
+    });
+    
+    app.get('/livros',function(request, response){
+        response.send(`
+             <html>
+                <head>
+                    <meta charset="utf-8":
+                </head>
+                <body>
+                    <h1>Listagem de Livros</h1>
+                </body>
+            </html>
+        `);
+    });
+}
